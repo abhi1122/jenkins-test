e9a7df36f681a110a08827316ef88ced319ae478
abhi1122 <abhi199036@gmail.com>
2021-07-30 15:20:59 +0530

    Update test.js

--- backend/test.js
+++ backend/test.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  console.log('call.........................);
+  console.log('call.........................');
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(300); //the server object listens on port 8080
